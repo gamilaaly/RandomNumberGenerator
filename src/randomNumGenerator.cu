f89be71e3a9855aee6133af68e8b246eac927a9d
Gamila <gamila.shafik97@eng-st.cu.edu.eg>
2019-09-26 01:53:41 +0200

    plot modified

--- src/randomNumGenerator.cu
+++ src/randomNumGenerator.cu
@@ -2,31 +2,23 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
-using namespace std;
-
-/* we need these includes for CUDA's random number stuff */
 #include <curand.h>
 #include <curand_kernel.h>
+using namespace std;
 
 #define N 500
-
 #define MAX 2000
 
-void streamOut (int *cpu_nums);
-
-/* this GPU kernel function is used to initialize the random states */
-__global__ void init(unsigned int seed, curandState_t* states) {
-
-  /* we have to initialize the state */
-  curand_init(seed, /* the seed can be the same for each core, here we pass the time in from the CPU */
-              blockIdx.x, /* the sequence number should be different for each core (unless you want all
-                             cores to get the same sequence of numbers for some reason - use thread id! */
-              0, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
-              &states[blockIdx.x]);
-}
+void streamOut (int *hostNums);
 
 /* this GPU kernel takes an array of states, and an array of ints, and puts a random int into each */
-__global__ void randoms(curandState_t* states, int* numbers) {
+__global__ void randoms(unsigned int seed, curandState_t* states, int* numbers) {
+  // initialize the random states
+   curand_init(seed, /* the seed can be the same for each core, here we pass the time in from the CPU */
+    blockIdx.x, /* the sequence number should be different for each core (unless you want all
+                   cores to get the same sequence of numbers for some reason - use thread id! */
+    0, /* the offset is how much extra we advance in the sequence for each call, can be 0 */
+    &states[blockIdx.x]);
   /* curand works like rand - except that it takes a state as a parameter */
   numbers[blockIdx.x] = curand(&states[blockIdx.x]) % MAX;
 }
@@ -35,46 +27,33 @@ int main() {
   /* CUDA's random number library uses curandState_t to keep track of the seed value
      we will store a random state for every thread  */
   curandState_t* states;
-
-  /* allocate space on the GPU for the random states */
   cudaMalloc((void**) &states, N * sizeof(curandState_t));
+  int *hostNums= (int*)malloc(sizeof(int) * N);
+  int* deviceNums;
+  cudaMalloc((void**) &deviceNums, N * sizeof(int));
 
-  /* invoke the GPU to initialize all of the random states */
-  init<<<N, 1>>>(time(0), states);
+  randoms<<<N, 1>>>( time(0), states, deviceNums);
 
-  /* allocate an array of unsigned ints on the CPU and GPU */
-  int cpu_nums[N];
-  int* gpu_nums;
-  cudaMalloc((void**) &gpu_nums, N * sizeof(int));
+  cudaMemcpy(hostNums, deviceNums, N * sizeof( int), cudaMemcpyDeviceToHost);
 
-  /* invoke the kernel to get some random numbers */
-  randoms<<<N, 1>>>(states, gpu_nums);
+  streamOut(&hostNums[0]);
 
-  //copy data from device array to host array
-  cudaMemcpy(cpu_nums, gpu_nums, N * sizeof( int), cudaMemcpyDeviceToHost);
-
- // for (int i = 0; i < N; i++) {
-  //  printf("%u\n", cpu_nums[i]);
- // }
-
-  streamOut(&cpu_nums[0]);
-
-  /* free the memory we allocated for the states and numbers */
   cudaFree(states);
-  cudaFree(gpu_nums);
+  cudaFree(deviceNums);
+  free(hostNums);
 
   return 0;
 }
 
-void streamOut(int *cpu_nums)
+void streamOut(int *hostNums)
 {
     std::ofstream resultFile;
     resultFile.open("randomNum.txt");
     if (resultFile.is_open())
-    {
+    {   resultFile << MAX << endl;
         for (int i = 0; i <N ; i++)
         {
-            resultFile << cpu_nums[i] << endl;
+            resultFile << hostNums[i] << endl;
         }
         resultFile.close();
     }
