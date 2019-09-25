4e71e8d665f913e23d221c9732f9efed6b454b2c
Gamila <gamila.shafik97@eng-st.cu.edu.eg>
2019-09-26 01:57:45 +0200

    plot modified

---
+++ src/plot.py
@@ -0,0 +1,25 @@
+
+import matplotlib.pyplot as plt
+
+dataarr = []
+indecies = []
+
+f = open("randomNum.txt", "r")
+data = f.readlines() #list
+data.pop(0)  #to remove the maximun number from the array
+data = [int(i) for i in data] #string to int
+totalNums = len(data) -1
+Max = data[0]
+
+for i in data:
+    dataarr.append(i)
+    indecies.append(data.index(i))
+
+
+    
+print(dataarr)
+print(indecies)
+
+plt.axis([0, totalNums, 0, Max])
+plt.plot(indecies,data, 'b.')
+plt.show()
