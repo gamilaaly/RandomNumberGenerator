4498f22adcd96378f82cda52ef1034033ab785bc
gamilaaly <gamila.shafik97@eng-st.cu.edu.eg>
2020-04-14 07:06:58 +0200

    RNG is ready!

---
+++ plotting/plot_uniform_gaussian.py
@@ -0,0 +1,28 @@
+from pylab import show,hist,subplot,figure
+from numpy import asarray
+import math
+
+uniformdataarr = []
+gaussiandataarr1=[]
+gaussiandataarr2 =[]
+
+f = open("randomNum.txt", "r")
+data = f.readlines()  # list
+data = [float(i) for i in data]  # string to float
+N=len(data)
+uniformdata=data[0:math.floor(N/2)]
+gaussiandata1=data[math.floor(N/2)+1 :math.floor(N-(N/4)) ]
+gaussiandata2=data[math.floor(N-(N/4)) +1 : N]
+
+uniformdataarr= asarray(uniformdata)
+gaussiandataarr1= asarray(gaussiandata1)
+gaussiandataarr2= asarray(gaussiandata2)
+
+figure()
+subplot(311) # the first row of graphs
+hist(uniformdataarr)     # contains the histograms of u1 and u2
+subplot(312)
+hist(gaussiandataarr1,1000)
+subplot(313) # the second contains
+hist(gaussiandataarr2,1000)    # the histograms of z1 and z
+show()
